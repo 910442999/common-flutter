d23b6ba2518aa0a1d8c7bf5b4267ea3281993a37
xuanwu <910442999@qq.com>
2023-04-23 00:28:22 +0800

    合并代码

--- lib/widget/chip_widget.dart
+++ lib/widget/chip_widget.dart
@@ -6,40 +6,43 @@ import '/res/colors.dart';
  */
 Widget ChipWidget(BuildContext context, String name, bool isSelected,
     {Color? borderColor,
-    Color unBorderColor = Colours.bg_gray,
-    Color textColor = Colours.text_gray,
-    double fontSize = 12,
-    bool border = true, //边框or填充
-    EdgeInsetsGeometry? padding}) {
-  Color primaryColor = Theme.of(context).primaryColor;
+      Color unBorderColor = Colours.bg_gray,
+      Color? textColor,
+      Color unTextColor = Colours.text_gray,
+      double fontSize = 12,
+      BorderRadiusGeometry? borderRadius,
+      bool border = true, //边框or填充
+      EdgeInsetsGeometry? padding}) {
+  Color primaryColor = Theme
+      .of(context)
+      .primaryColor;
   return Container(
-    alignment: Alignment.center,
     padding: padding,
     decoration: isSelected
         ? border
-            ? BoxDecoration(
-                border:
-                    Border.all(width: 1, color: borderColor ?? primaryColor),
-                borderRadius: BorderRadius.circular(20.0),
-              )
-            : BoxDecoration(
-                color: borderColor ?? primaryColor,
-                borderRadius: BorderRadius.circular(20.0),
-              )
+        ? BoxDecoration(
+      border:
+      Border.all(width: 1, color: borderColor ?? primaryColor),
+      borderRadius: borderRadius ?? BorderRadius.circular(20.0),
+    )
+        : BoxDecoration(
+      color: borderColor ?? primaryColor,
+      borderRadius: borderRadius ?? BorderRadius.circular(20.0),
+    )
         : border
-            ? BoxDecoration(
-                border: Border.all(width: 1, color: unBorderColor),
-                borderRadius: BorderRadius.circular(20.0),
-              )
-            : BoxDecoration(
-                color: unBorderColor,
-                borderRadius: BorderRadius.circular(20.0),
-              ),
+        ? BoxDecoration(
+      border: Border.all(width: 1, color: unBorderColor),
+      borderRadius: borderRadius ?? BorderRadius.circular(20.0),
+    )
+        : BoxDecoration(
+      color: unBorderColor,
+      borderRadius: borderRadius ?? BorderRadius.circular(20.0),
+    ),
     child: Text(
       name,
       textAlign: TextAlign.center,
       style: TextStyle(
-          color: isSelected ? Colours.material_bg : Colours.text_gray,
+          color: isSelected ? textColor ?? primaryColor : Colours.text_gray,
           fontSize: fontSize),
     ),
   );
