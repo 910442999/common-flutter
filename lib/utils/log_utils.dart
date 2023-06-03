d36b9bb70456a932d22a6512db72756bca7aad04
xuanwu <910442999@qq.com>
2023-06-04 01:27:35 +0800

    合并代码

--- lib/utils/log_utils.dart
+++ lib/utils/log_utils.dart
@@ -6,33 +6,33 @@ import 'package:flutter/foundation.dart';
 class Log {
   static const String tag = 'LOG-UTILS';
 
-  static void d(String msg, {String tag = tag}) {
+  static void v(String? msg, {String tag = tag, int level = 100}) {
     if (kDebugMode) {
-      _printLog(tag, ' d ', msg);
+      _printLog(tag, level, msg);
     }
   }
 
-  static void v(String? msg, {String tag = tag}) {
+  static void d(String msg, {String tag = tag, int level = 500}) {
     if (kDebugMode) {
-      _printLog(tag, ' v ', msg);
+      _printLog(tag, level, msg);
     }
   }
 
-  static void e(String? msg, {String tag = tag}) {
+
+  static void e(String? msg, {String tag = tag, int level = 1000}) {
     if (kDebugMode) {
-      _printLog(tag, ' e ', msg);
+      _printLog(tag, level, msg);
     }
   }
 
-  static void json(String msg, {String tag = tag}) {
+  static void json(String msg, {String tag = tag, int level = 1500}) {
     if (kDebugMode) {
-      _printLog(tag, ' json ', "\n\n" + msg + "\n\n");
+      _printLog(tag, level, "\n\n" + msg + "\n\n");
     }
   }
 
-  static void _printLog(String? tag, String stag, Object? object) {
+  static void _printLog(String? tag, int level, Object? object) {
     String da = object?.toString() ?? 'null';
-    tag = tag ?? tag;
-    log('$tag$stag $da');
+    log(da, name: tag ?? Log.tag, level: level);
   }
 }
