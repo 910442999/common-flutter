973d94f7e3326eb63702982daa3bbec137f748df
xuanwu <910442999@qq.com>
2023-06-04 01:07:14 +0800

    合并代码

--- lib/utils/log_utils.dart
+++ lib/utils/log_utils.dart
@@ -1,10 +1,10 @@
 import 'dart:convert' as convert;
+import 'dart:developer';
 import 'package:flutter/foundation.dart';
 
 /// 输出Log工具类
 class Log {
   static const String tag = 'LOG-UTILS';
-  static const int _maxLen = 5000;
 
   static void d(String msg, {String tag = tag}) {
     if (kDebugMode) {
@@ -33,22 +33,6 @@ class Log {
   static void _printLog(String? tag, String stag, Object? object) {
     String da = object?.toString() ?? 'null';
     tag = tag ?? tag;
-    if (da.length <= _maxLen) {
-      print('$tag$stag $da');
-      return;
-    }
-    print(
-        '$tag$stag — — — — — — — — — — — — — — — — st — — — — — — — — — — — — — — — —');
-    while (da.isNotEmpty) {
-      if (da.length > _maxLen) {
-        print(da.substring(0, _maxLen));
-        da = da.substring(_maxLen, da.length);
-      } else {
-        print(da);
-        da = '';
-      }
-    }
-    print(
-        '$tag$stag — — — — — — — — — — — — — — — — ed — — — — — — — — — — — — — — — —');
+    log('$tag$stag $da');
   }
 }
