95ba369572a2f12d132c26ec7ca052057885525d
xuanwu <910442999@qq.com>
2023-09-09 01:46:02 +0800

    合并代码

--- lib/res/dimens.dart
+++ lib/res/dimens.dart
@@ -7,6 +7,7 @@ class Dimens {
   static const double font_sp18 = 18.0;
   static const double font_sp20 = 20.0;
   static const double font_sp22 = 22.0;
+  static const double font_sp30 = 30.0;
 
   static const double gap_dp2 = 2;
   static const double gap_dp4 = 4;
